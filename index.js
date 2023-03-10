fb3ad2c0fb154373d5142bae80bf058e2b4af47d
shridhar027 <127202129+shridhar027@users.noreply.github.com>
2023-03-10 15:34:35 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+let username;
+document.getElementById("button1").onclick = function(){
+    name=document.getElementById("text1").value;
+    reg_no=document.getElementById("text2").value;
+    branch=document.getElementById("text3").value;
+    place=document.getElementById("text4").value;
+    console.log(name);
+    console.log(reg_no);
+    console.log(branch);
+    console.log(place);
+}
